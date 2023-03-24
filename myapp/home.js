df1c2d9beeb51f523d1ca7f9ca75a4b3bdcf703e
Etudiant <toure.hdmpro@gmail.com>
2023-03-24 10:16:56 +0100

    Modification mineur -> employees

--- myapp/home.js
+++ myapp/home.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();// Exemple de données d'employés
 
 const employees = [
- { id: 1, firstName: 'HDM', lastName: 'Toure', emailId: 'hdm.toure@example.com' },
- { id: 2, firstName: 'Corto', lastName: 'Bruzd', emailId: 'Corto.bruzd@example.com' },
- { id: 3, firstName: 'Rayan', lastName: 'Din', emailId: 'ryan.din@example.com' }
+	{ id: 1, firstName: 'HDM', lastName: 'Toure', emailId: 'hdm.toure@example.com' },
+	{ id: 2, firstName: 'Corto', lastName: 'Bruzda', emailId: 'Corto.bruzd@example.com' },
+	{ id: 3, firstName: 'Rayan', lastName: 'Din', emailId: 'ryan.din@example.com' },
+	{ id: 4, firstName: 'Kadero', lastName: 'Jams', emailId: 'kadero.jams@example.com' },
+	{ id: 5, firstName: 'Celia', lastName: 'Jamsy', emailId: 'Celia.jmsy@example.com' }
+	
 ];
 
 // Route pour récupérer tous les employés == READ
@@ -49,11 +52,11 @@ app.put('/api/v1/employees/:id', (req, res) => {
 	const { id } = req.params;
 	const employeeIndex = employees.findIndex(emp => emp.id === parseInt(id));
 	if (employeeIndex === -1) {
-	  return res.status(404).json({ message: `Employee with id ${id} not found` });
+	  return res.status(404).json({ message: `Employe avec l'id ${id} n'est pas trouver` });
 	}
 	const { firstName, lastName, emailId } = req.body;
 	if (!firstName || !lastName || !emailId) {
-	  return res.status(400).json({ message: 'All fields are required' });
+	  return res.status(400).json({ message: 'Tous les champs sont requis' });
 	}
 	const updatedEmployee = {
 	  id: parseInt(id),
@@ -69,7 +72,7 @@ app.put('/api/v1/employees/:id', (req, res) => {
   app.post('/api/v1/employees', (req, res) => {
 	const { firstName, lastName, emailId } = req.body;
 	if (!firstName || !lastName || !emailId) {
-	  return res.status(400).json({ message: 'All fields are required' });
+	  return res.status(400).json({ message: 'Tous les champs sont requis' });
 	}
 	const newEmployee = {
 	  id: employees.length + 1,
